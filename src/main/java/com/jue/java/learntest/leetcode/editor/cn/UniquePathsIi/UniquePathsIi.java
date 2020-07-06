5f0b989fb82afd986b52c2aa38d59bd550ccaff5
wangjueya <573711282@qq.com>
2020-07-06 19:28:15 +0800

    # 2020-07-06 每日一练

---
+++ src/main/java/com/jue/java/learntest/leetcode/editor/cn/UniquePathsIi/UniquePathsIi.java
@@ -0,0 +1,68 @@
+//一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。 
+//
+// 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。 
+//
+// 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？ 
+//
+// 
+//
+// 网格中的障碍物和空位置分别用 1 和 0 来表示。 
+//
+// 说明：m 和 n 的值均不超过 100。 
+//
+// 示例 1: 
+//
+// 输入:
+//[
+//  [0,0,0],
+//  [0,1,0],
+//  [0,0,0]
+//]
+//输出: 2
+//解释:
+//3x3 网格的正中间有一个障碍物。
+//从左上角到右下角一共有 2 条不同的路径：
+//1. 向右 -> 向右 -> 向下 -> 向下
+//2. 向下 -> 向下 -> 向右 -> 向右
+// 
+// Related Topics 数组 动态规划 
+// 👍 357 👎 0
+
+
+package com.jue.java.learntest.leetcode.editor.cn.UniquePathsIi;
+
+/**
+ * @author JUE
+ * @number 63
+ */
+public class UniquePathsIi {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int row = obstacleGrid.length;
+        int col = obstacleGrid[0].length;
+        int[][] dp = new int[row][col];
+        dp[0][0] = 1 - obstacleGrid[0][0];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    if (i > 0) {
+                        dp[i][j] += dp[i - 1][j];
+                    }
+                    if (j > 0) {
+                        dp[i][j] += dp[i][j - 1];
+                    }
+                }
+            }
+        }
+        return dp[row - 1][col - 1];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
