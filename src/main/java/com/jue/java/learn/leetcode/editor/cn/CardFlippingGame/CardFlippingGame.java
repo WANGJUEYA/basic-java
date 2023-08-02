3dc61f376564055fce1b0e823031e1409dcdf571
wangjueya <573711282@qq.com>
2023-08-02 09:46:35 +0800

    :sparkles: 2023-08-02 不同数字包含自己本来的背面, 及一张牌如果有正反面, 则不通过

---
+++ src/main/java/com/jue/java/learn/leetcode/editor/cn/CardFlippingGame/CardFlippingGame.java
@@ -0,0 +1,86 @@
+//在桌子上有 N 张卡片，每张卡片的正面和背面都写着一个正数（正面与背面上的数有可能不一样）。
+//
+// 我们可以先翻转任意张卡片，然后选择其中一张卡片。
+//
+// 如果选中的那张卡片背面的数字 X 与任意一张卡片的正面的数字都不同，那么这个数字是我们想要的数字。
+//
+// 哪个数是这些想要的数字中最小的数（找到这些数中的最小值）呢？如果没有一个数字符合要求的，输出 0。
+//
+// 其中, fronts[i] 和 backs[i] 分别代表第 i 张卡片的正面和背面的数字。
+//
+// 如果我们通过翻转卡片来交换正面与背面上的数，那么当初在正面的数就变成背面的数，背面的数就变成正面的数。
+//
+// 示例：
+//
+//
+//输入：fronts = [1,2,4,4,7], backs = [1,3,4,1,3]
+//输出：2
+//解释：假设我们翻转第二张卡片，那么在正面的数变成了 [1,3,4,4,7] ， 背面的数变成了 [1,2,4,1,3]。
+//接着我们选择第二张卡片，因为现在该卡片的背面的数是 2，2 与任意卡片上正面的数都不同，所以 2 就是我们想要的数字。
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= fronts.length == backs.length <= 1000
+// 1 <= fronts[i] <= 2000
+// 1 <= backs[i] <= 2000
+//
+//
+// Related Topics 数组 哈希表 👍 38 👎 0
+
+
+package com.jue.java.learn.leetcode.editor.cn.CardFlippingGame;
+
+import java.util.*;
+
+/**
+ * @author JUE
+ * @number 822
+ */
+public class CardFlippingGame {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.flipgame(new int[]{1}, new int[]{1}));
+    }
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    Map<Integer, List<Integer>> numIdx;
+
+    public int flipgame(int[] fronts, int[] backs) {
+        int len = fronts.length;
+        numIdx = new HashMap<>(len);
+        for (int idx = 0; idx < len; idx++) {
+            numIdx.putIfAbsent(fronts[idx], new ArrayList<>());
+            numIdx.get(fronts[idx]).add(idx);
+            numIdx.putIfAbsent(backs[idx], new ArrayList<>());
+            numIdx.get(backs[idx]).add(idx);
+        }
+        Integer[] nums = numIdx.keySet().toArray((new Integer[0]));
+        Arrays.sort(nums);
+        // 不能用二分查找法, 在于排序好的数字并不能线性逼近
+        for (int num : nums) {
+            if (check(num)) {
+                return num;
+            }
+        }
+        return 0;
+    }
+
+    private boolean check(int number) {
+        // 如果这个数字在一张牌正反面都出现了(且出现的索引不是自己本身; 包含自己本身), 则不能校验通过
+        List<Integer> idxes = numIdx.get(number);
+        // 出现了两次一样的数据遍认为不通过
+        for (int i = 1, len = idxes.size(); i < len; i++) {
+            if (idxes.get(i).equals(idxes.get(i - 1))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
