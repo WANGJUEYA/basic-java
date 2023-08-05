2d942ea57fafc36a11088773d0b9d9d8c028c906
wangjueya <573711282@qq.com>
2023-08-05 16:58:14 +0800

    :sparkles: 2023-08-05; 使用非递归方案, 按逻辑合并两个数组

--- src/main/java/com/jue/java/learn/leetcode/editor/cn/MergeTwoSortedLists/MergeTwoSortedLists.java
+++ src/main/java/com/jue/java/learn/leetcode/editor/cn/MergeTwoSortedLists/MergeTwoSortedLists.java
@@ -38,21 +38,37 @@ public class MergeTwoSortedLists {
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode head = null;
-        if (l1 == null) {
-            head = l2;
-        } else if (l2 == null) {
-            head = l1;
-        } else {
-            if (l1.val < l2.val) {
-                head = l1;
-                head.next = mergeTwoLists(l1.next, l2);
+        ListNode result = null;
+        ListNode next = null;
+        ListNode head1 = l1;
+        ListNode head2 = l2;
+        while (head1 != null && head2 != null) {
+            ListNode tmp;
+            if (head1.val < head2.val) {
+                tmp = head1;
+                head1 = head1.next;
             } else {
-                head = l2;
-                head.next = mergeTwoLists(l1, l2.next);
+                tmp = head2;
+                head2 = head2.next;
             }
+            if (result == null) {
+                result = tmp;
+            }
+            if (next == null) {
+                next = tmp;
+            } else {
+                next.next = tmp;
+                next = next.next;
+            }
+
         }
-        return head;
+
+        if (result == null || next == null) {
+            return head1 == null ? head2 : head1;
+        } else {
+            next.next = head1 == null ? head2 : head1;
+            return result;
+        }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
